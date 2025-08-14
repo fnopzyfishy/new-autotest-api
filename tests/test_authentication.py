05e1bbd1f1756f14ebbdcbd28529a5bbd60a64bc
fnopzyfishy <perec2742@gmail.com>
2025-08-14 22:20:21 +0500

    pytest marks done

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
 
+import pytest
+
 from clients.authentication.authentication_client import get_authentication_client
 from clients.users.public_users_client import get_public_users_client
 from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
@@ -8,6 +10,8 @@ from tools.assertions.base import assert_status_code
 from tools.assertions.authentication import assert_login_response
 from tools.assertions.schema import validate_json_schema
 
+@pytest.mark.regression
+@pytest.mark.authentication
 def test_login():
     public_users_client = get_public_users_client()
     authentication_user = get_authentication_client()
