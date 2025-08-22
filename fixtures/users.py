b00bf2ebca86384997a0e36dd22b2500b98bd640
fnopzyfishy <perec2742@gmail.com>
2025-08-22 21:07:30 +0500

    plugins done

--- fixtures/users.py
+++ fixtures/users.py
@@ -1,11 +1,11 @@
 import pytest
 from pydantic import BaseModel, EmailStr
 
-from clients.authentication.authentication_client import AuthenticationClient, get_authentication_client
 from clients.private_http_builder import AuthenticationUserSchema
-from clients.users.private_users_client import PrivateUsersClient, get_private_users_client
-from clients.users.public_users_client import PublicUsersClient, get_public_users_client
+from clients.users.private_users_client import get_private_users_client, PrivateUsersClient
+from clients.users.public_users_client import get_public_users_client, PublicUsersClient
 from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
+
 
 class UserFixture(BaseModel):
     request: CreateUserRequestSchema
@@ -21,23 +21,18 @@ class UserFixture(BaseModel):
 
     @property
     def authentication_user(self) -> AuthenticationUserSchema:
-        return AuthenticationUserSchema(
-            email=self.request.email,
-            password=self.request.password
-        )
+        return AuthenticationUserSchema(email=self.email, password=self.password)
 
-
-@pytest.fixture
-def authentication_client() -> AuthenticationClient:
-    return get_authentication_client()
 
 @pytest.fixture
 def public_users_client() -> PublicUsersClient:
     return get_public_users_client()
 
+
 @pytest.fixture
-def private_users_client(function_user) -> PrivateUsersClient:
+def private_users_client(function_user: UserFixture) -> PrivateUsersClient:
     return get_private_users_client(function_user.authentication_user)
+
 
 @pytest.fixture
 def function_user(public_users_client: PublicUsersClient) -> UserFixture:
