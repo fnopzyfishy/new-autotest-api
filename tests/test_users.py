2b5f5b0ff5d7a861b8ffddee99a2faceb0ff7dfe
fnopzyfishy <perec2742@gmail.com>
2025-08-23 17:33:40 +0500

    parametrize done

--- tests/test_users.py
+++ tests/test_users.py
@@ -7,12 +7,14 @@ from clients.users.users_schema import CreateUserRequestSchema, CreateUserRespon
 from tools.assertions.schema import validate_json_schema
 from tools.assertions.base import assert_status_code
 from tools.assertions.users import assert_create_user_response
+from tools.faker import fake
 
 
 @pytest.mark.users
 @pytest.mark.regression
-def test_create_user(public_users_client: PublicUsersClient):
-    request = CreateUserRequestSchema()
+@pytest.mark.parametrize("domain", ["mail.ru", "gmail.com", "example.com"])
+def test_create_user(public_users_client: PublicUsersClient, domain: str):
+    request = CreateUserRequestSchema(email=fake.email(domain=domain))
     response = public_users_client.create_user_api(request)
     response_data = CreateUserResponseSchema.model_validate_json(response.text)
 
